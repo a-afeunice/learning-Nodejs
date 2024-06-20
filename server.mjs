c195c62d2985c6324cc5bca8880638499c293cd4
a-afeunice <afrifae2@gmail.com>
2024-06-20 13:24:18 +0000

    File system in Nodejs

--- server.mjs
+++ server.mjs
@@ -1,19 +1,35 @@
-import{createServer} from 'node:http';
+import { createServer } from 'node:http';
 
 
 
 
 // Create HTTP Server
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>We have an HTTP Server</h1>');
+    console.log(req.url);
+    if (req.url.includes('create')) {
+        // Create file
+        writeFile('./abc.html', '<h1>Learning Node.js</h1>', (err) => {
+            console.log('err');
+            // Return response
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>We have an HTTP Server</h1>');
+        });
+    } else {
+        // Delete file
+        unlink('./hello.html', () => {
+            // Return response
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>We have an HTTP Server</h1>');
+        });
+    }
 });
 
+
 // Listen for incoming request
-server.listen(3000, '127.0.0.1',  () => {
+server.listen(3000, '127.0.0.1', () => {
+
     console.log('Server is running');
 });
 
-// Normal function vs Arrow function
-function handleRequest() {}
-const handleRequest = () => {}
+// Arrow function
+const handleRequest = () => { }
